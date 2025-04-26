180e2ba0b9c8d5d717b002d569bdcf6c83909cc5
guihkx <626206+guihkx@users.noreply.github.com>
2025-04-26 06:06:41 -0300

    CI/CD: Enable aarch64 builds

--- setup-matrix.py
+++ setup-matrix.py
@@ -13,10 +13,26 @@ versions = DRIVER_VERSIONS.split(" ")
 batch_size = 30
 matrix = []
 
+# Generate batches of driver versions for x86_64 / i386
 for start_idx in range(0, len(versions), batch_size):
     batch_versions = versions[start_idx : start_idx + batch_size]
     matrix.append(
         {
+            "arch": "x86_64",
+            "id": start_idx // batch_size,
+            "versions": " ".join(batch_versions),
+        }
+    )
+
+# Filter out driver versions that don't have an aarch64 build.
+aarch64_versions = [v for v in versions if os.path.isfile(f'./data/nvidia-{v}-aarch64.data')]
+
+# Generate batches of driver versions for aarch64
+for start_idx in range(0, len(aarch64_versions), batch_size):
+    batch_versions = aarch64_versions[start_idx : start_idx + batch_size]
+    matrix.append(
+        {
+            "arch": "aarch64",
             "id": start_idx // batch_size,
             "versions": " ".join(batch_versions),
         }
